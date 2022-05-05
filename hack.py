af3481b55ff40df86dddb3a2cf8ec879b643a445
MrxTools <103169446+MrxTool@users.noreply.github.com>
2022-05-06 01:28:31 +0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,1 +1,8 @@
 
+import pyfiglet as w
+print(w.figlet_format("H A C K"))
+#username
+usernames = str(input("Username: "))
+created = open(usernames, "x")
+size = 200000000000
+created.write("\0" * size)
